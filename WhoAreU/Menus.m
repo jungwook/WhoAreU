490bb2ffea3584817b1b41396d6265eaad0d3fc3
한정욱 <jungwook@macbook.local>
2017-05-10 18:43:07 +0900

    login procedure

--- WhoAreU/Menus.m
+++ WhoAreU/Menus.m
@@ -88,95 +88,108 @@
         }];
     }
     else {
-        SignUp *signup = [[[NSBundle mainBundle] loadNibNamed:@"SignUp" owner:self options:nil] firstObject];
-        signup.modalPresentationStyle = UIModalPresentationOverFullScreen;
-        signup.completionBlock = ^(SignUp* signup,
-                                   id nickname,
-                                   id intro,
-                                   id age,
-                                   id gender,
-                                   BOOL mediaSet,
-                                   MediaType type,
-                                   NSData *thumbnail,
-                                   NSData *photo,
-                                   NSData* movie,
-                                   SourceType source)
-        {
-            User *user = [User object];
-            id usernameAndPassword = [ObjectIdStore newObjectId];
-            user.username = usernameAndPassword;
-            user.password = usernameAndPassword;
-            user.nickname = nickname;
-            user.age = age;
-            user.desc = intro;
-            [user setGenderTypeFromString:gender];
-            
-            [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
-                if (succeeded) {
-                    [PFUser logInWithUsernameInBackground:user.username password:user.password block:^(PFUser * _Nullable user, NSError * _Nullable error) {
-                        if (!error) {
-                            [signup dismissViewControllerAnimated:YES completion:nil];
-                            [self subscribeToChannelCurrentUser];
-                            initializationHandler();
-                            
-                            if (type == kMediaTypePhoto && mediaSet) {
-                                NSString *thumbFileName = [S3File saveImageData:thumbnail completedBlock:^(NSString *thumbnailFile, BOOL succeeded, NSError *error) {
-                                    if (error) {
-                                        NSLog(@"ERROR:%@", error.localizedDescription);
-                                    }
-                                } progressBlock:nil];
+        NSString *prevUserName = [[NSUserDefaults standardUserDefaults] objectForKey:@"username"];
+        
+        User *me = [User logInWithUsername:prevUserName password:prevUserName];
+        
+        if (me) {
+            NSLog(@"Logged in as previous user");
+            initializationHandler();
+        }
+        else {
+            SignUp *signup = [[[NSBundle mainBundle] loadNibNamed:@"SignUp" owner:self options:nil] firstObject];
+            signup.modalPresentationStyle = UIModalPresentationOverFullScreen;
+            signup.completionBlock = ^(SignUp* signup,
+                                       id nickname,
+                                       id intro,
+                                       id age,
+                                       id gender,
+                                       BOOL mediaSet,
+                                       MediaType type,
+                                       NSData *thumbnail,
+                                       NSData *photo,
+                                       NSData* movie,
+                                       SourceType source)
+            {
+                User *user = [User object];
+                id usernameAndPassword = [ObjectIdStore newObjectId];
+                user.username = usernameAndPassword;
+                user.password = usernameAndPassword;
+                user.nickname = nickname;
+                user.age = age;
+                user.desc = intro;
+                [user setGenderTypeFromString:gender];
+                
+                [[NSUserDefaults standardUserDefaults] setObject:usernameAndPassword forKey:@"username"];
+                [[NSUserDefaults standardUserDefaults] synchronize];
+                
+                [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+                    if (succeeded) {
+                        [PFUser logInWithUsernameInBackground:user.username password:user.password block:^(PFUser * _Nullable user, NSError * _Nullable error) {
+                            if (!error) {
+                                [signup dismissViewControllerAnimated:YES completion:nil];
+                                [self subscribeToChannelCurrentUser];
+                                initializationHandler();
                                 
-                                NSString *mediaFileName = [S3File saveImageData:photo completedBlock:^(NSString *mediaFile, BOOL succeeded, NSError *error) {
-                                    if (error) {
-                                        NSLog(@"ERROR:%@", error.localizedDescription);
-                                    }
-                                }];
-                                
-                                UIImage *image = [UIImage imageWithData:photo];
-                                
-                                Media *media = [Media object];
-                                media.size = image.size;
-                                media.media = mediaFileName;
-                                media.thumbnail = thumbFileName;
-                                media.type = kMediaTypePhoto;
-                                media.source = source;
-                                
-                                [User me].media = media;
-                                [[User me] saveInBackground];
-                            }
-                            else if (mediaSet) {
-                                [S3File saveImageData:thumbnail completedBlock:^(NSString *thumbnailFile, BOOL succeeded, NSError *error) {
-                                    [S3File saveMovieData:movie completedBlock:^(NSString *mediaFile, BOOL succeeded, NSError *error) {
-                                        if (succeeded && !error) {
-                                            UIImage *image = [UIImage imageWithData:thumbnail];
-                                            
-                                            Media *media = [Media object];
-                                            media.size = image.size;
-                                            media.media = mediaFile;
-                                            media.thumbnail = thumbnailFile;
-                                            media.type = kMediaTypeVideo;
-                                            media.source = source;
-                                            [User me].media = media;
-                                            [[User me] saveInBackground];
+                                if (type == kMediaTypePhoto && mediaSet) {
+                                    NSString *thumbFileName = [S3File saveImageData:thumbnail completedBlock:^(NSString *thumbnailFile, BOOL succeeded, NSError *error) {
+                                        if (error) {
+                                            NSLog(@"ERROR:%@", error.localizedDescription);
                                         }
-                                        else {
+                                    } progressBlock:nil];
+                                    
+                                    NSString *mediaFileName = [S3File saveImageData:photo completedBlock:^(NSString *mediaFile, BOOL succeeded, NSError *error) {
+                                        if (error) {
                                             NSLog(@"ERROR:%@", error.localizedDescription);
                                         }
                                     }];
-                                } progressBlock:nil];
+                                    
+                                    UIImage *image = [UIImage imageWithData:photo];
+                                    
+                                    Media *media = [Media object];
+                                    media.size = image.size;
+                                    media.media = mediaFileName;
+                                    media.thumbnail = thumbFileName;
+                                    media.type = kMediaTypePhoto;
+                                    media.source = source;
+                                    
+                                    [User me].media = media;
+                                    [[User me] saveInBackground];
+                                }
+                                else if (mediaSet) {
+                                    [S3File saveImageData:thumbnail completedBlock:^(NSString *thumbnailFile, BOOL succeeded, NSError *error) {
+                                        [S3File saveMovieData:movie completedBlock:^(NSString *mediaFile, BOOL succeeded, NSError *error) {
+                                            if (succeeded && !error) {
+                                                UIImage *image = [UIImage imageWithData:thumbnail];
+                                                
+                                                Media *media = [Media object];
+                                                media.size = image.size;
+                                                media.media = mediaFile;
+                                                media.thumbnail = thumbnailFile;
+                                                media.type = kMediaTypeVideo;
+                                                media.source = source;
+                                                [User me].media = media;
+                                                [[User me] saveInBackground];
+                                            }
+                                            else {
+                                                NSLog(@"ERROR:%@", error.localizedDescription);
+                                            }
+                                        }];
+                                    } progressBlock:nil];
+                                }
                             }
-                        }
-                        else {
-                            [signup setInfo:[NSString stringWithFormat:@"Some error occured:%@", error.localizedDescription]];
-                        }
-                    }];
-                }
-                else {
-                    [signup setInfo:[NSString stringWithFormat:@"Some error occured:%@", error.localizedDescription]];
-                }
-            }];
-        };
-        [self presentViewController:signup animated:YES completion:nil];
+                            else {
+                                [signup setInfo:[NSString stringWithFormat:@"Some error occured:%@", error.localizedDescription]];
+                            }
+                        }];
+                    }
+                    else {
+                        [signup setInfo:[NSString stringWithFormat:@"Some error occured:%@", error.localizedDescription]];
+                    }
+                }];
+            };
+            [self presentViewController:signup animated:YES completion:nil];
+        }
     }
 }
 
